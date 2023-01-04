ef044186852df6f587f472df8b251b0bed2b3dd4
Shristi Hamal <shristihamal@gmail.com>
2023-01-04 19:29:08 +0545

    file updated

--- src/array/TestArray.java
+++ src/array/TestArray.java
@@ -3,7 +3,7 @@ package array;
 public class TestArray {
 	
 	public static void main(String[] args) {
-		System.out.println("testing");
+		System.out.println("testing git ");
 		System.out.println("Test Array");
 	}
 
